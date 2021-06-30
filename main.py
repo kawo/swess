4270a9647e34e6fa5680372abcf622baa85ed6f0
Kévin Audebrand <kevin.audebrand@gmail.com>
2021-07-01 00:30:38 +0200

    renamed main to swess

--- main.py
+++
@@ -1,9 +0,0 @@
-"""main loop"""
-
-
-def main():
-    return print("OK!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ swess.py
@@ -0,0 +1,12 @@
+from controllers.base import BaseController
+
+
+def main():
+    """main loop"""
+
+    swess = BaseController()
+    swess.showMainMenu()
+
+
+if __name__ == "__main__":
+    main()
